1378198ab00972676029f54260d16745c0be1ee3
Thota Anil <anil@meghplat.com>
2024-10-15 14:58:58 +0530

    URl Updated

--- js/script.js
+++ js/script.js
@@ -33,7 +33,7 @@ document.addEventListener("DOMContentLoaded", function () {
             return;
         }
 
-        fetch('http://localhost:3001/project')
+        fetch('https://purple-backend-362176970036.asia-south1.run.app/project')
             .then(response => response.json())
             .then(data => {
                 const projects = data.projects;
@@ -81,7 +81,7 @@ function updateLogo() {
 	}
   
 	// Fetch the logo data from the API using the project ID
-	fetch(`http://localhost:3001/properties/${PROJECTID}/logo`)
+	fetch(`https://purple-backend-362176970036.asia-south1.run.app/properties/${PROJECTID}/logo`)
 	  .then(response => response.json())
 	  .then(data => {
 		console.log("API Response:", data);
@@ -102,7 +102,7 @@ function updateLogo() {
 }
 
 function updateAddressDetails() {
-	fetch(`http://localhost:3001/properties/${PROJECTID}/address`)
+	fetch(`https://purple-backend-362176970036.asia-south1.run.app/properties/${PROJECTID}/address`)
 		.then(response => response.json())
 		.then(data => {
 			if (data) {
@@ -139,7 +139,7 @@ function updateAddressDetails() {
 
 function updateAboutSection() {
 	// Fetching API data for the About Us section
-	fetch(`http://localhost:3001/properties/${PROJECTID}/about`)
+	fetch(`https://purple-backend-362176970036.asia-south1.run.app/properties/${PROJECTID}/about`)
 		.then(response => response.json())
 		
 		.then(data => {
@@ -173,7 +173,7 @@ let swiperInstance = null;
 
 
 function updateCarouselSlides() {
-  fetch(`http://localhost:3001/properties/${PROJECTID}/banner`)
+  fetch(`https://purple-backend-362176970036.asia-south1.run.app/properties/${PROJECTID}/banner`)
     .then(response => response.json())
     .then(data => {
       if (data.banners && data.banners.length > 0) {
@@ -217,7 +217,7 @@ function updateCarouselSlides() {
 }
 
 function updateAboutUsBanner() {
-	const apiUrl = `http://localhost:3001/properties/${PROJECTID}/banner`;
+	const apiUrl = `https://purple-backend-362176970036.asia-south1.run.app/properties/${PROJECTID}/banner`;
 
 	// Fetch banner data from the API using Fetch
 	fetch(apiUrl)
@@ -263,7 +263,7 @@ function updateAboutUsBanner() {
 }
 
 function updateContactsBanner(){
-	const apiUrl = `http://localhost:3001/properties/${PROJECTID}/banner`;
+	const apiUrl = `https://purple-backend-362176970036.asia-south1.run.app/properties/${PROJECTID}/banner`;
 	// Fetch banner data from the API using Fetch
 	fetch(apiUrl)
 	  .then(response => {
@@ -308,7 +308,7 @@ function updateContactsBanner(){
   }
 
   function updateTypographyBAnner(){
-	const apiUrl = `http://localhost:3001/properties/${PROJECTID}/banner`;
+	const apiUrl = `https://purple-backend-362176970036.asia-south1.run.app/properties/${PROJECTID}/banner`;
 	// Fetch banner data from the API using Fetch
 	fetch(apiUrl)
 	  .then(response => {
